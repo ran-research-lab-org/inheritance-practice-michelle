6aa362b15703e28f4d3e6993811050c73651273b
michelle-angelic <michelle.santiago19@upr.edu>
2025-09-06 21:30:00 +0000

    ok so i had to make my own cmake?? idk how this woks but i found like the most basic ass template of making a cmake txt file bc it wasnt letting me run it?? anyway, found it, added the executables, and now lets see if it actually compiles

---
+++ CMakeFiles/inheritance-practice-michelle.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/inheritance-practice-michelle/Employee.cpp" "CMakeFiles/inheritance-practice-michelle.dir/Employee.cpp.o" "gcc" "CMakeFiles/inheritance-practice-michelle.dir/Employee.cpp.o.d"
+  "/workspaces/inheritance-practice-michelle/HourlyEmployee.cpp" "CMakeFiles/inheritance-practice-michelle.dir/HourlyEmployee.cpp.o" "gcc" "CMakeFiles/inheritance-practice-michelle.dir/HourlyEmployee.cpp.o.d"
+  "/workspaces/inheritance-practice-michelle/SalariedEmployee.cpp" "CMakeFiles/inheritance-practice-michelle.dir/SalariedEmployee.cpp.o" "gcc" "CMakeFiles/inheritance-practice-michelle.dir/SalariedEmployee.cpp.o.d"
+  "/workspaces/inheritance-practice-michelle/main.cpp" "CMakeFiles/inheritance-practice-michelle.dir/main.cpp.o" "gcc" "CMakeFiles/inheritance-practice-michelle.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
