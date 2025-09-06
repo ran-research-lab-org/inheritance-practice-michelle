51bc57cc91ec1e1d5f4f8e98555c1b7f0f71e208
michelle-angelic <michelle.santiago19@upr.edu>
2025-09-06 22:18:38 +0000

    so i had a dozen more compiler issues, mostly from the absolute nightmare that was setting up a cmake file. anyways. i set it up. it worked. i have no idea what i just did, but special thanks to my brother who helped me set it up

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/Employee.cpp.o"
+  "CMakeFiles/main.dir/Employee.cpp.o.d"
+  "CMakeFiles/main.dir/HourlyEmployee.cpp.o"
+  "CMakeFiles/main.dir/HourlyEmployee.cpp.o.d"
+  "CMakeFiles/main.dir/SalariedEmployee.cpp.o"
+  "CMakeFiles/main.dir/SalariedEmployee.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
