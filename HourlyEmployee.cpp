7ebdd97e50862111371500216b34bf434d6bdd69
michelle-angelic <michelle.santiago19@upr.edu>
2025-09-06 20:31:43 +0000

    oka so esto es la implementacion de hourlyemployee. im not tweaking that much. i think.

--- HourlyEmployee.cpp
+++ HourlyEmployee.cpp
@@ -1,9 +1,24 @@
 #include "HourlyEmployee.h"
 
-HourlyEmployee::HourlyEmployee(const std::string& name, int id, 
-                                double payPerHour, double workedHours)
+HourlyEmployee::HourlyEmployee(const std::string& name, int id, double payPerHour, double workedHours) : Employee(name, id, "Hourly")
 {
-    // TODO: 
+    this->payPerHour = payPerHour;
+    this->workedHours = workedHours;
 }
 /*TODO: calculate Payment */
 
+double HourlyEmployee::calculatePay () {
+    double hourly = getPayPerHour();
+    double timeWorked = getWorkedHours();
+
+    double pay = hourly * timeWorked;
+    return pay;
+}
+
+double HourlyEmployee::getPayPerHour () {
+    return payPerHour;
+}
+
+double HourlyEmployee::getWorkedHours () {
+    return workedHours;
+}
