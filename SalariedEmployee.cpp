6b4a2feb5ecf70e602d371fb5d912ac1f99091d2
michelle-angelic <michelle.santiago19@upr.edu>
2025-09-06 20:42:57 +0000

    salaried employee (like the defs and implementations)

--- SalariedEmployee.cpp
+++ SalariedEmployee.cpp
@@ -1,9 +1,20 @@
 #include "SalariedEmployee.h"
 
-SalariedEmployee::SalariedEmployee(const std::string& name, int id, double yearlyPayment)
-: {
-    // TODO: 
+SalariedEmployee::SalariedEmployee(const std::string& name, int id, double yearlyPayment): Employee(name, id, "Salaried") 
+{
+    this->yearlyPayment = yearlyPayment;
 }
+
 /*TODO: calculate Payment */
 
+double SalariedEmployee::calculatePay () {
+    double yearly = getYearlyPayment();
+    double biweekly = yearly / 26;
+    return biweekly;
+}
+
 /* IMPLEMENT GETTERS*/
+
+double SalariedEmployee:: getYearlyPayment() const {
+    return yearlyPayment;
+}
